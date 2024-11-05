0b75a8824886ead222a9e5014f24bbc325132f73
WanQijian <qijianwan@sf-express.com>
2024-11-05 21:01:31 +0800

    add marscode 001

---
+++ MarsCode/001-数字分组求偶数和/solution.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    # Please write your code here
+    return -1
+
+if __name__ == "__main__":
+    #  You can add more test cases here
+    print(solution([123, 456, 789]) == 14)
+    print(solution([123456789]) == 4)
+    print(solution([14329, 7568]) == 10)
